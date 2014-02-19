8a123b1003a86f2c597a90c5bc89c2311cbf10f8
Charlon Palacay <charlon@gmail.com>
2014-02-19 10:56:14 -0800

    Updates client url.py to handle all routes.

--- angulartest/client/urls.py
+++ angulartest/client/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, include, url
 #from django.contrib import admin
 #admin.autodiscover()
 
+
+# direct all routes through the default view
 urlpatterns = patterns('',
-
-    url(r'^/?$', 'client.views.home'),
-
+    url(r'', 'client.views.home'),
 )
