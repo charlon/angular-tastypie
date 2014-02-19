008da7e2ce8b7ec07ae5173fcd3eb402d3eaea6a
Charlon Palacay <charlon@gmail.com>
2014-02-19 10:45:21 -0800

    Adds client app urls.py

--- angulartest/client/views.py
+++ angulartest/client/views.py
@@ -3,3 +3,8 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 # Create your views here.
+def home(request, template_name="index.html"):
+    """
+    A index view.
+    """
+    return render_to_response(template_name, context_instance=RequestContext(request))
