0521e6f535244b9d2bab04f3da6170c834e106df
Charlon Palacay <charlon@gmail.com>
2014-03-05 12:36:10 -0800

    Adds badges page and view for hybrid app.

--- frameworks/hybrid/urls.py
+++ frameworks/hybrid/urls.py
@@ -6,13 +6,13 @@ urlpatterns = patterns('',
     # routing for pjax app
     
     # /pjax/about
-    #url(r'^/about', 'hybrid.views.about'),
+    url(r'^/about', 'hybrid.views.about'),
     
     # /pjax/test
-    #url(r'^/test', 'hybrid.views.test'),
+    url(r'^/test', 'hybrid.views.test'),
     
     # /pjax/badges
-    #url(r'^/badges', 'hybrid.views.badges'),
+    url(r'^/badges', 'hybrid.views.badges'),
     
     # /pjax/anything
     url(r'^', 'hybrid.views.index'),
--- frameworks/hybrid/views.py
+++ frameworks/hybrid/views.py
@@ -6,3 +6,12 @@ from django.shortcuts import render_to_response
 
 def index(request, template_name="hybrid/index.html"):
     return render_to_response(template_name, context_instance=RequestContext(request))
+
+def about(request, template_name="hybrid/about.html"):
+    return render_to_response(template_name, context_instance=RequestContext(request))
+    
+def test(request, template_name="hybrid/test.html"):
+    return render_to_response(template_name, context_instance=RequestContext(request))
+
+def badges(request, template_name="hybrid/badges.html"):
+    return render_to_response(template_name, context_instance=RequestContext(request))
