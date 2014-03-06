9fd7700fa8f0101473a3e1f67f1171ced9f5da33
Charlon Palacay <charlon@gmail.com>
2014-03-05 17:01:33 -0800

    Getting closer to displaying via handlebars.

--- frameworks/hybrid/static/hybrid/js/main.js
+++ frameworks/hybrid/static/hybrid/js/main.js
@@ -1,14 +1,13 @@
 $(function() {
     
-    var properties = {
-        total: 10,
-        min: 4,
-        max: 5
-    };
+    var stuff = $('#badge_list_container').data('badges');
+    var badgestuff = jQuery.parseJSON(stuff);
+    
+    console.log(badgestuff);
 
     // compile handlebars template
     var template = Handlebars.compile($('#tpl-badge-list').html()),
-        rendered = template(properties);
+        rendered = template(badgestuff);
                 
     // paint it where? at the end of the content
     //$("#badge_list_container").html(rendered);
