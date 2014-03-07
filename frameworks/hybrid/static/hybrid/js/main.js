97bcc9c431cc716bad556916a17b4b63220e441a
Charlon Palacay <charlon@gmail.com>
2014-03-07 12:04:57 -0800

    Only fire handlebars compile and render on proper page.

--- frameworks/hybrid/static/hybrid/js/main.js
+++ frameworks/hybrid/static/hybrid/js/main.js
@@ -1,22 +1,29 @@
 $(function() {
     
-    // get the badges json from the data-badges attribute
-    var stuff = $('#badge_list_container').data('badges');
     
-    // give context
-    var context = { badges: stuff };
+    // check to see if the badge list container exists... if so, start the display process via handlebars    
+    if( $("#badge_list_container" ).length > 0) {
+    
 
-    // compile handlebars template and render
-    var template = Handlebars.compile($('#tpl-badge-list').html()),
-        rendered = template(context);
-                
-    // simulate a delayed rendering time
-    setTimeout(function () {
-    
-        // paint it in the badge list container
-        $("#badge_list_container").html(rendered);
-    
-    }, 400);
+      // get the badges json from the data-badges attribute
+        var stuff = $('#badge_list_container').data('badges');
         
+        // give context
+        var context = { badges: stuff };
+    
+        // compile handlebars template and render
+        var template = Handlebars.compile($('#tpl-badge-list').html()),
+            rendered = template(context);
+                                        
+        // delay the rendering of badges
+        setTimeout(function () {
+        
+            // paint it in the badge list container
+            $("#badge_list_container").html(rendered);
+        
+        }, 100);
+
+    }
+    
         
 });
