e6b2db53467706522904322bda51add7eda4c586
Charlon Palacay <charlon@gmail.com>
2014-03-08 19:59:10 -0800

    Sets a timer on page load and displays loading message if exceeded.

--- frameworks/pjax/static/pjax/js/main.js
+++ frameworks/pjax/static/pjax/js/main.js
@@ -1,21 +1,32 @@
 $(function() {
     
-    $(document).pjax('[data-pjax] a, a[data-pjax]', '#pjax-container')
+    $(document).pjax('[data-pjax] a, a[data-pjax]', '#pjax-container',  {timeout: 10000})
     
-    $(document).on("pjax:beforeSend", function(e) {
-        $("#pjax-loading").removeClass("hidden");
-        $('#pjax-container').hide();
+    $(document).on("pjax:send", function(e) {
+        
+        
+        // if it's taken longer than 250ms... show the loading message & hide existing content
+          timer = setTimeout(function() {
+                $("#pjax-loading").show();
+                $('#pjax-container').hide();
+          }, 250);
+    
     });
 
     $(document).on("pjax:complete", function() {
-        $("#pjax-loading").addClass("hidden");
+                
+        // once pjax completes... hide the loading message and make sure the content is showing    
+        $("#pjax-loading").hide();
         $('#pjax-container').show();
+        
+        // cancel showing the message when the ajax call completes.
+        clearTimeout(timer);
+        
     });
     
     $(document).on('pjax:timeout', function(event) {
         // Prevent default timeout redirection behavior
-        event.preventDefault()
+        event.preventDefault();
     })
-    
     
 });
