e28fdfd3a734680ba4585e2027621449b583efe6
Charlon Palacay <charlon@gmail.com>
2014-03-11 21:39:51 -0700

    Adds load timer for pjax views.

--- frameworks/pjax/static/pjax/js/main.js
+++ frameworks/pjax/static/pjax/js/main.js
@@ -1,9 +1,13 @@
 $(function() {
     
+    var startTime, endTime, millisecondsLoading;
+    
     $(document).pjax('[data-pjax] a, a[data-pjax]', '#pjax-container',  {timeout: 10000})
     
     $(document).on("pjax:send", function(e) {
         
+        // start the timer on the initial pjax request
+        startTime = (new Date()).getTime();
         
         // if it's taken longer than 250ms... show the loading message & hide existing content
           loadingTimeout = setTimeout(function() {
@@ -22,11 +26,24 @@ $(function() {
         // cancel showing the message when the ajax call completes.
         clearTimeout(loadingTimeout);
         
+        // calculate total loading time
+        endTime = (new Date()).getTime();
+        millisecondsLoading = endTime - startTime;
+        
+        if(millisecondsLoading >= 1000) {
+            // convert to seconds        	
+        	millisecondsLoading = (millisecondsLoading / 1000) + " s";
+        } 
+        else {
+        	millisecondsLoading = millisecondsLoading + " ms";
+        }
+        
+        $("#load_timer").html(millisecondsLoading);
     });
     
     $(document).on('pjax:timeout', function(event) {
         // Prevent default timeout redirection behavior
         event.preventDefault();
     })
-    
+       
 });
