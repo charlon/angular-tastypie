3cd63c66526e3ea41760d0087defc1b0656e3034
Charlon Palacay <charlon@gmail.com>
2014-03-12 10:30:24 -0700

    Put loading of badge data into function.

--- frameworks/hybrid/static/hybrid/js/main.js
+++ frameworks/hybrid/static/hybrid/js/main.js
@@ -15,45 +15,56 @@ $(function() {
         }
         
         $("#load_timer").html(millisecondsLoading);
+        
+        // check to see if the badge list container exists... if so, start loading the badges
+        if( $("#badge_list_container" ).length > 0) {
+            loadBadges();
+        }
             
     });
-        
-    // check to see if the badge list container exists... if so, start the display process via handlebars    
-    if( $("#badge_list_container" ).length > 0) {
-    
-
-      // get the badges json from the data-badges attribute
-        var badge_json = $('#badge_list_container').data('badges');
-        
-        // give context
-        var context = { badges: badge_json };
-    
-        // compile handlebars template and render
-        var template = Handlebars.compile($('#tpl-badge-list').html()),
-            rendered = template(context);
-                                        
-        // delay the rendering of badges
-        setTimeout(function () {
-        
-            // paint it in the badge list container
-            $("#badge_list_container").html(rendered);
-                        
-            // calculate total loading time
-            endTime = (new Date()).getTime();
-            millisecondsLoading = endTime - startTime;
-            
-            if(millisecondsLoading >= 1000) {
-                // convert to seconds        	
-            	millisecondsLoading = (millisecondsLoading / 1000) + " s";
-            } 
-            else {
-            	millisecondsLoading = millisecondsLoading + " ms";
-            }
-            
-            $("#hb_timer").html(millisecondsLoading);
-        
-        }, 10);
-
-    }
- 
+         
 });
+
+function loadBadges() {
+    
+    console.log("start loading badges");
+            
+    // start the timer
+    var hybridStart, hybridEnd, hybridMilliseconds;
+    hybridStart = (new Date()).getTime();    
+    
+    console.log(hybridStart);
+
+    // get the badges json from the data-badges attribute
+    var badge_json = $('#badge_list_container').data('badges');
+    
+    // give context
+    var context = { badges: badge_json };
+
+    // compile handlebars template and render
+    var template = Handlebars.compile($('#tpl-badge-list').html()),
+        rendered = template(context);
+                                    
+    // delay the rendering of badges
+    setTimeout(function () {
+    
+        // paint it in the badge list container
+        $("#badge_list_container").html(rendered);
+                    
+        // calculate total loading time
+        hybridEnd = (new Date()).getTime();
+        hybridMilliseconds = hybridEnd - startTime;
+        
+        if(hybridMilliseconds >= 1000) {
+            // convert to seconds        	
+        	hybridMilliseconds = (hybridMilliseconds / 1000) + " s";
+        } 
+        else {
+        	hybridMilliseconds = hybridMilliseconds + " ms";
+        }
+        
+        $("#hb_timer").html(hybridMilliseconds);
+    
+    }, 10);
+            
+}
