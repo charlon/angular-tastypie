b7c2461ae57be9479675a57df9ad43f66ac4c533
Charlon Palacay <charlon@gmail.com>
2014-04-02 16:14:13 -0700

    Updates stampos model to support pagination via rest framework,

--- frameworks/stampos/models.py
+++ frameworks/stampos/models.py
@@ -8,32 +8,22 @@ class Badge(models.Model):
     badge_class = models.CharField(max_length=100)
     badge_pin = models.IntegerField(max_length=10)
 
-    #def json_data(self):
-    #    data = {'id': self.pk,
-    #            'name': self.name,
-    #            'description': self.description,
-    #            'presenters': self.presenters,
-    #            'badge_class' : self.badge_class}
-    #    return data
-
 class User(models.Model):
     netid = models.CharField(max_length=8, unique=True)
     name = models.CharField(max_length=100)
     user_photo = models.CharField(max_length=100)
-
-    #def json_data(self):
-    #    data = {'id': self.pk,
-    #            'name': self.name,
-    #            'netid': self.netid,
-    #            'photo_path': self.user_photo}
-    #    return data
-
 
 class BadgeAward(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     badge = models.ForeignKey('Badge')
     user = models.ForeignKey('User')
     
-    #def json_data(self):
-    #    return {"badge_class": self.badge.badge_class}
+
+from rest_framework import serializers
+
+class BadgeSerializer(serializers.ModelSerializer):
     
+    class Meta:
+        model = Badge
+        fields = ('id', 'name', 'description')
+
--- frameworks/stampos/views.py
+++ frameworks/stampos/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 
-from .models import Badge
+from .models import Badge, BadgeSerializer
 
 # Create your views here.
 
 from rest_framework import generics
 
-
 class BadgeList(generics.ListCreateAPIView):
-    model = Badge
 
+    queryset = Badge.objects.all()
+    serializer_class = BadgeSerializer
+    paginate_by = 30
+    
 class BadgeDetail(generics.RetrieveUpdateDestroyAPIView):
     model = Badge
+
