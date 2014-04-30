3c3b24cbfd2a5a3e80586e533b760296f9bf0b24
Charlon Palacay <charlon@gmail.com>
2014-04-29 23:06:58 -0700

    Adds comment on command to run for offline compression for production.

--- frameworks/frameworks/settings/production.py
+++ frameworks/frameworks/settings/production.py
@@ -13,6 +13,10 @@ STATIC_ROOT = None
 
 # django compressor and less-c compiler (compiles Less using LessC and translates url() paths)
 
+# run...
+# python manage.py collectstatic
+# python manage.py compress
+
 COMPRESS_OFFLINE = True
 COMPRESS_ENABLED = True
 COMPRESS_PRECOMPILERS = (('text/less', 'lessc {infile} {outfile}'),)
